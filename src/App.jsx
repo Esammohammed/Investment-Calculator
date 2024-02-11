15d5eaf50e1440c07f26dc29916cd69c1552869d
Esammohammed <essamonly111@gmail.com>
2024-02-11 23:13:56 +0200

    Update a App component and add Header,UserInput and Resultes to it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-function App() {
+import React from 'react';
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput..jsx';
+import { useState } from "react"
+import Results from './components/Results.jsx';
+
+export default function App() {
+  const [userInput, setUserInput] = useState({ initialInvestment: 1000, annualInvestment: 1200, expectedReturn: 6, duration: 40 })
+  function handleChange(inputIdentefer, value) {
+    setUserInput((prev) => {
+        return {
+            ...prev,
+            [inputIdentefer]:+ value
+        }
+    })
+
+
+}
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+    <Header/>
+    <UserInput handleChange = {handleChange} userInput = {userInput}/>
+    <Results input = {userInput}/>
+    </>
+  );
 }
 
-export default App
